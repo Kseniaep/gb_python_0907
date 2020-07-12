34971c20536d02cf10e7785e558787e9f1eb141c
kseniaep <ksuxampei@yandex.ru>
2020-07-12 12:19:11 +0300

    Task 2 done

--- homework/lesson1/task1.py
+++ homework/lesson1/task1.py
@@ -1,1 +1,8 @@
 "Home work"
+"Task 1"
+
+name = input("Введите Ваше имя: ")
+print ("Вас зовут ", name)
+num = int(input("Введите целое число: "))
+print ("Вы ввели число ", num)
+print ("Квадрат этого числа ", num ** 2)
