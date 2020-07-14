6427fa10f78f58483acb14db5b5396506511ece5
kseniaep <ksuxampei@yandex.ru>
2020-07-14 18:49:13 +0300

    Homework done

--- lesson2/task3.py
+++ lesson2/task3.py
@@ -2,10 +2,11 @@ autumn = [9, 10, 11]
 winter = [12, 1, 2]
 spring = [3, 4, 5]
 summer = [6, 7, 8]
-period = dict(autumn = [9,10,11], winter=[1,2,12], spring = [3,4,5], summer = [6,7,8])
 
 month = int(input('Введите номер месяца от 1 до 12 '))
-if month in autumn:
+if (month < 1) or (month > 12):
+    print('Значение введено неверно')
+elif month in autumn:
     print('Осень')
 elif month in winter:
     print('Зима')
@@ -13,9 +14,12 @@ elif month in spring:
     print('Весна')
 elif month in summer:
     print('Лето')
-else:
-    print('Значение введено неверно')
 
-for key, value in period.items():
-    if month in value:
-        print(key)
+period = dict(autumn = [9,10,11], winter=[1,2,12], spring = [3,4,5], summer = [6,7,8])
+month = int(input('Введите номер месяца от 1 до 12 '))
+if (month < 1) or (month > 12):
+    print('Значение введено неверно')
+else:
+    for key, value in period.items():
+        if month in value:
+            print(key)
